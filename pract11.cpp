a5f26ea5aebc60c61c18d1c585cc480685b4ea62
Damir <karamov-damir228@mail.ru>
2026-01-19 18:03:46 +0500

    initial commit

---
+++ pract11.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+
+#define MAX_LOGS 1000
+#define MAX_TEXT 128
+char logs[][MAX_TEXT] = {"log1","log2","log3","log4","log5"};
+DWORD tick = GetTickCount64();
+
+struct LogRecord
+{
+	DWORD threadId;
+	int priority;
+	DWORD tick;
+
+	char message[MAX_TEXT];
+};
+
+struct LogBuffer 
+{
+	LogRecord records[MAX_LOGS];
+	LONG index;
+};
+
+LogBuffer buffer;
+CRITICAL_SECTION cs;
+
+DWORD WINAPI Logger(DWORD priority)
+{;
+	srand(time(NULL));
+	while (buffer.index <= MAX_LOGS)
+	{
+
+		EnterCriticalSection(&cs);
+
+		LogRecord log;
+		log.threadId = GetCurrentThreadId();
+		log.priority = priority;
+		log.tick = GetTickCount64() - tick;
+
+		strcpy_s(log.message, logs[rand() % 5]);
+
+		buffer.records[buffer.index] = log;
+		buffer.index++;
+
+		LeaveCriticalSection(&cs);
+
+		Sleep(rand() % 100 + 10);
+	};
+	return 0;
+}
+DWORD WINAPI Watcher()
+{
+	do
+	{
+		EnterCriticalSection(&cs);
+		int index = buffer.index - 5;
+		for (int i = 0; i < 5; i++)
+		{
+			if (index <= 0)
+				break;
+			LogRecord log = buffer.records[index + i];
+			std::cout << "[" << index + i << "]" <<
+				" priority: " << log.priority <<
+				" message: " << log.message <<
+				" tick: " << log.tick <<
+				std::endl;
+		}
+		std::cout << std::endl;
+		LeaveCriticalSection(&cs);
+		Sleep(100);
+	} while (buffer.index <= MAX_LOGS);
+
+	return 0;
+}
+
+int main() //логи теряются и выводятся не все, с критической секцией они все работают нормально
+{
+
+	InitializeCriticalSection(&cs);
+
+	std::cout << "e to stop" << std::endl;
+
+	DWORD IDThread1, IDThread2, IDThread3, IDThread4;
+	HANDLE highPriorityLogger = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Logger, (void*)THREAD_PRIORITY_HIGHEST, 0, &IDThread1);
+
+	if (highPriorityLogger == NULL)
+		return GetLastError();
+	SetPriorityClass(highPriorityLogger, THREAD_PRIORITY_HIGHEST);
+
+	HANDLE normalPriorityLogger = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Logger, (void*)THREAD_PRIORITY_NORMAL, 0, &IDThread2);
+	if (normalPriorityLogger == NULL)
+		return GetLastError();
+	SetPriorityClass(normalPriorityLogger, THREAD_PRIORITY_NORMAL);
+
+	HANDLE lowPriorityLogger = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Logger, (void*)THREAD_PRIORITY_BELOW_NORMAL, 0, &IDThread3);
+	if (lowPriorityLogger == NULL)
+		return GetLastError();
+	SetPriorityClass(lowPriorityLogger, THREAD_PRIORITY_BELOW_NORMAL);
+
+	HANDLE watcher = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Watcher, (void*)THREAD_PRIORITY_LOWEST, 0, &IDThread4);
+	if (watcher == NULL)
+		return GetLastError();
+	SetPriorityClass(watcher, THREAD_PRIORITY_LOWEST);
+
+	WaitForSingleObject(highPriorityLogger, INFINITE);
+	WaitForSingleObject(normalPriorityLogger, INFINITE);
+	WaitForSingleObject(lowPriorityLogger, INFINITE);
+	WaitForSingleObject(watcher, INFINITE);
+
+	CloseHandle(highPriorityLogger);
+	CloseHandle(normalPriorityLogger);
+	CloseHandle(lowPriorityLogger);
+	CloseHandle(watcher);
+}
